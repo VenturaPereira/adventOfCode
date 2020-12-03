cde04e035f1f9ba391a11871446408d3a72e0551
VenturaPereira <venturasousapereira@hotmail.com>
2020-12-03 10:04:59 +0000

    day 3 done

--- AdventCode/DayOne.cs
+++ AdventCode/DayOne.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace AdventCode
 {
@@ -20,6 +21,13 @@ namespace AdventCode
             int result4 = dayTwo.getValidPasswordsTwo(input2);
             Console.WriteLine(result3);
             Console.WriteLine(result4);
+            string[] input3 = readOriginalText(@"C:\workspace\adventOfCode\AdventCode\inputDayThree.txt");
+            DayThree dayThree = new DayThree();
+            int result5 = dayThree.trajectory(input3,3,1);
+            int[,] slopes = new int[,] {{1,1},{3,1},{5,1},{7,1},{1,2}};
+            int result6 = dayThree.trajectoryTwo(input3,slopes);
+            Console.WriteLine(result5);
+            Console.WriteLine(result6);
         }
 
         static int[] readFile(string path){
@@ -128,4 +136,44 @@ namespace AdventCode
 
 
     }
+
+    public class DayThree{
+
+        public int trajectory(string[] input, int x, int y){
+            var result = 0;
+            var xIndex = x;
+            var down = y;
+            char letter;
+            for(int i=down; i < input.Length; i = i + down){
+                if(input[i].Length <= xIndex){
+                    int multiplier = xIndex/input[i].Length;
+                    string pattern = String.Concat(Enumerable.Repeat(input[i],multiplier+1));
+                    letter = pattern[xIndex];
+                   
+                }else{
+                    letter = input[i][xIndex];
+                }
+                if(letter == '#'){
+                    result++;
+                }
+                xIndex = xIndex+x;  
+            }
+            return result;
+        }
+
+
+        public int trajectoryTwo(string[] input, int[,] slope){
+            var result = 1;
+            for(int i = 0; i < slope.GetLength(0); i++){
+                int trees = trajectory(input, slope[i,0], slope[i,1]);
+                result = result * trees;
+            }
+            return result;
+        }
+
+
+
+    }
+
+
 }
